5abde0b8b9b3ca0bb542413ab8b287a47bf4b93d
unknown <mertalibarin@gmail.com>
2021-02-17 14:28:55 +0300

    Type Assertions and interfaces

---
+++ Functions/functions.js
@@ -0,0 +1,24 @@
+function getAverage() {
+    var a = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        a[_i] = arguments[_i];
+    }
+    var total = 0;
+    var count = 0;
+    for (var i = 0; i < a.length; i++) {
+        total += a[i];
+        count++;
+    }
+    var result = total / count;
+    return 'result :' + result;
+}
+getAverage(10, 20, 30);
+getAverage(10, 20);
+getAverage(10, 20, 30, 40, 50);
+getAverage(10, 20);
+// Arrow
+var getAverage2 = function () {
+    return "";
+};
+var getAverage3 = function () {
+};
